4d5f9abf71990a18dbf4a1122669b8932c5b2f63
jercon <jercon.mahinay@gmail.com>
2020-11-05 00:15:34 -0500

    update the script with steps

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,10 +1,25 @@
 import argparse
 
-ap=argparse.ArgumentParser()
-ap.add_argument("-s", "--start_date", help="start date format yyyy-mm-dd")
-ap.add_argument("-e", "--end_date", help="end date format yyyy-mm-dd")
-ap.add_argument("-y", "--symbol", required=True, help="stock Symbol")
+#class LookBack():
+#	def compare_value():
 
-args=vars(ap.parse_args())
+#class DataExtraction():
+#"will extract data for all stock code or stock details"
+#	def extract_symbols():
+#	def extract_stock_data():
 
-print("symbol is {}".format(str(args['symbol'])))
+def arg_parser():
+	ap=argparse.ArgumentParser()
+	ap.add_argument("-s", "--start_date", require=True, help="start date format yyyy-mm-dd")
+	ap.add_argument("-e", "--end_date", required=True, help="end date format yyyy-mm-dd")
+	ap.add_argument("-y", "--symbol", required=True, help="stock Symbol")
+
+	args=vars(ap.parse_args())
+
+	print("symbol is {}".format(str(args['symbol'])))
+
+def main():
+	arg_parser()
+
+#if __name__ = main()
+	main()
